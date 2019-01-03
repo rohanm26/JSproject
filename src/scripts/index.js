40cb2d9c91af2d88b3112c980c5a806d0d98f456
rohanm26 <mandharerohan6@gmail.com>
2019-01-03 18:04:36 +0530

    project upload

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,17 @@
-import '../styles/index.scss';
 
-console.log('webpack starterkit');
+
+document.write(typeof(1)+"<br>");
+document.write(typeof(true)+"<br>");
+document.write(typeof('hello')+"<br>");
+document.write(typeof(function(){})+"<br>");
+document.write(typeof({})+"<br>");
+document.write(typeof(null)+"<br>");
+document.write(typeof(NaN)+"<br>");
+
+let id= 123;
+document.write("123" != id);
+
+
+
+
+
